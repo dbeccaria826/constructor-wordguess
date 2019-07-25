55aaf2e660c2bd2106f9ea1fdd59ae851d8c8d2a
Dan Beccaria <beccdaniel@gmail.com>
2019-07-24 20:09:38 -0400

    generating random blanks working

--- Words.js
+++ Words.js
@@ -3,7 +3,13 @@ function Word (word) {
     this.splitWords = []
 }
 
-let newWord = new Word('chicken')
+//Start with an array of word choices.
+//Theme: Syonyms for lazy
+const wordChoices = ['apathetic','careless','inattentive','lackadasical','weary','slothful','snoozy','laggard'
+,'comatose','dallying','drowsy','lifeless','slack','unindustrious','unconcerned','unpreserving','unready'];
+
+const randomChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+let newWord = new Word(randomChoice)
 
 Word.prototype.getLetters = function () {
     let array = this.word.split("")
--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const Words = require("./Words")
 //Start with an array of word choices.
 //Theme: Syonyms for lazy
 
-const wordChoices = ['apathetic','careless','inattentive','lackadasical','weary','slothful','snoozy','laggard'
-,'comatose','dallying','drowsy','lifeless','slack','unindustrious','unconcerned','unpreserving','unready'];
 
-const randomChoice = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 
 
 
